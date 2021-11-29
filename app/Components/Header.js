25cb44485ea5e30ee0b161a0b04d8f81b282323a
ekrem35 <anliekrem35@gmail.com>
2021-11-29 20:23:35 +0300

    added goBack button and title prop to header

--- app/Components/Header.js
+++ app/Components/Header.js
@@ -1,20 +1,48 @@
+/**
+ * @flow
+ **/
 import React from 'react'
-import { Header as NBHeader, Left, Right, Body, Title } from 'native-base'
+import { Header as NBHeader, Left, Right, Body, Title, Icon } from 'native-base'
+import { TouchableOpacity } from 'react-native'
+import { goBack } from '../Router/RootNavigation'
 
-const Header = () => {
+type Props = {
+  home: Boolean,
+  title: String
+}
+
+const Header = (props: Props) => {
+  const { home, title } = props
   return (
     <NBHeader
       style={{ backgroundColor: '#1B1553' }}
       iosBarStyle="light-content"
       androidStatusBarColor="#1B1553"
     >
-      <Left></Left>
-      <Body>
-        <Title>uPhoto</Title>
+      <Left>
+        {home ? null : (
+          <TouchableOpacity onPress={goBack}>
+            <Icon type="AntDesign" name="left"></Icon>
+          </TouchableOpacity>
+        )}
+      </Left>
+      <Body
+        style={{
+          alignItems: 'center',
+          justifyContent: 'center',
+          minWidth: 90
+        }}
+      >
+        <Title>{title}</Title>
       </Body>
       <Right></Right>
     </NBHeader>
   )
+}
+
+Header.defaultProps = {
+  home: false,
+  title: 'uPhoto'
 }
 
 export default Header
