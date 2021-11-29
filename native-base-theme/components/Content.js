7d3567b5929b0f338852746a7e808b7b907e02ef
ekrem35 <anliekrem35@gmail.com>
2021-11-29 20:26:08 +0300

    added default padding to content component

--- native-base-theme/components/Content.js
+++ native-base-theme/components/Content.js
@@ -3,12 +3,13 @@
 export default () => {
   const contentTheme = {
     flex: 1,
+    padding: 24,
     backgroundColor: 'transparent',
     'NativeBase.Segment': {
       borderWidth: 0,
       backgroundColor: 'transparent'
     }
-  };
+  }
 
-  return contentTheme;
-};
+  return contentTheme
+}
