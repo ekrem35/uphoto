426b4e0eaecff4e09b56d946a3b5637c08c4ca54
ekrem35 <anliekrem35@gmail.com>
2021-11-29 20:25:20 +0300

    added function in order to be able to dispatch navigation actions from out of component

--- app/Router/Router.js
+++ app/Router/Router.js
@@ -2,12 +2,13 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Screens from '../Screens'
+import { navigationRef } from './RootNavigation'
 
 const Stack = createNativeStackNavigator()
 
 const Router = () => {
   return (
-    <NavigationContainer>
+    <NavigationContainer ref={navigationRef}>
       <Stack.Navigator
         screenOptions={{
           headerShown: false
