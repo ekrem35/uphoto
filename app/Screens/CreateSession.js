dc11e96853ba56d420b546b0a5255d2b32a9c425
ekrem35 <anliekrem35@gmail.com>
2021-11-30 20:54:24 +0300

    added image capture and upload actions

--- app/Screens/CreateSession.js
+++ app/Screens/CreateSession.js
@@ -1,50 +1,77 @@
 import React from 'react'
 import { Container, Content, View, Text, Button, Icon } from 'native-base'
 import Header from '../Components/Header'
-import { Dimensions, FlatList, StyleSheet } from 'react-native'
+import { Dimensions, Image, StyleSheet } from 'react-native'
+import { launchCamera } from 'react-native-image-picker'
+import { useDispatch, useSelector } from 'react-redux'
+import { saveImage } from '../state/actions/sessions'
+import Carousel from 'react-native-snap-carousel'
 
 const CreateSession = () => {
+  const dispatch = useDispatch()
+  const { images } = useSelector((state) => ({
+    images: state.storage.images
+  }))
+
+  const onPressAddPhoto = async () => {
+    const response = await launchCamera({
+      quality: 0.5,
+      mediaType: 'photo',
+      maxWidth: 1920,
+      maxHeight: 1080
+    })
+    response.assets.map((asset) => dispatch(saveImage(asset.uri)))
+  }
+
+  const onPressCreateSession = () => {
+    // dispatch(createSession())
+  }
+
   return (
     <Container>
       <Header title="Create Session" />
-      <Content>
-        <View style={styles.topView}>
+      <Content contentContainerStyle={{ flex: 1 }}>
+        <View style={styles.messageText}>
           <Text style={styles.noteText}>
-            Your images will be listed just below
+            {images.length
+              ? 'Uploaded Images'
+              : 'Your images will be listed just below'}
           </Text>
-          <FlatList
-            horizontal
-            data={[1, 2, 3, 4, 5, 6, 7, 8, 9, 0]}
-            style={{ height: Dimensions.get('screen').height / 3 }}
+        </View>
+        <View style={styles.carouselView}>
+          <Carousel
+            data={images}
+            sliderWidth={Dimensions.get('screen').width}
+            itemWidth={Dimensions.get('screen').width - 32}
             renderItem={({ item }) => (
-              <View
+              <Image
+                source={{ uri: item }}
+                resizeMode="contain"
                 style={{
-                  width: Dimensions.get('screen').width - 24,
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  borderWidth: 1,
-                  margin: 24
+                  width: Dimensions.get('screen').width - 32,
+                  height: 360
                 }}
-              >
-                <Text>{item}</Text>
-              </View>
+              />
             )}
-            keyExtractor={(item, index) => String(index)}
           />
         </View>
-        <View>
+        <View style={styles.completeButtonView}>
           <Button
-            style={{ height: Dimensions.get('screen').height / 3 }}
+            style={{ height: Dimensions.get('screen').height / 4 }}
             full
-            bordered
             iconLeft
+            onPress={onPressAddPhoto}
           >
             <Icon type="MaterialCommunityIcons" name="image-plus"></Icon>
             <Text>Add Photo</Text>
           </Button>
-        </View>
-        <View style={styles.completeButtonView}>
-          <Button full bordered iconLeft>
+
+          <Button
+            onPress={onPressCreateSession}
+            style={styles.completeButtonView}
+            full
+            iconLeft
+          >
             <Icon type="AntDesign" name="check"></Icon>
             <Text>Complete Session</Text>
           </Button>
@@ -55,8 +82,13 @@ const CreateSession = () => {
 }
 
 const styles = StyleSheet.create({
-  topView: {
-    justifyContent: 'center',
+  carouselView: {
+    marginTop: 24,
+    marginLeft: -24
+  },
+  messageText: {
+    backgroundColor: '#57A6FF',
+    paddingVertical: 12,
     alignItems: 'center'
   },
   noteText: {
